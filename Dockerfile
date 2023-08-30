53a69e9ad9a3235de81c7fce3640a8cf1e0f66ca
Andrey Grytsenko <andreyapg@gmail.com>
2023-08-30 14:36:25 +0300

    02 web app 2023-08-30

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,25 @@
-FROM golang:1.16
+FROM golang:latest
 
 WORKDIR /app
 
-COPY . .
+COPY . /app
 
 RUN go mod init main
 RUN go mod tidy
 RUN go build -o main .
 
 CMD ["./main"]
+
+
+# stop all
+# docker stop $(docker ps -a -q)
+# remove all
+# docker rm $(docker ps -qa)
+
+# remove all unused
+# docker container prune
+
+# remove all images
+# docker rmi $(docker images -q)
+
+# docker run --name=go-web-app -p 80:8080 go-app
--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
     "fmt"
-    // "net/http"
+    "net/http"
 )
 
-// func main() {
-//     http.HandleFunc("/docker", func(w http.ResponseWritter, req *http.Request) {
-//         fmt.Fprintf(w, "<h1>Hello from Docker container make by Golang!</h1>")
-//     })
-
-//     http.ListenAndServe(":8080", null)
-// }
-
 func main() {
-    fmt.Println("Hello, world!")
+    http.HandleFunc("/docker", func(w http.ResponseWriter, req *http.Request) {
+        fmt.Fprintf(w, "<h1>Hello from Docker container make by Golang!</h1>")
+    })
+
+    http.ListenAndServe(":8080", nil)
 }
 
-// docker run --name=go-web-app -p 80:8080 go-app
+// func main() {
+//     fmt.Println("Hello, world!")
+// }
+
